e7cbdf334df9aec45be4059029b957a2e546bc10
Kadir <60246846+khad1r@users.noreply.github.com>
2026-06-02 13:59:51 +0700

    removing autoload file and using built in composer dump autoload

--- html/app/init.php
+++ html/app/init.php
@@ -2,6 +2,5 @@
 if (file_exists('./vendor/autoload.php')) require_once './vendor/autoload.php';
 
 require_once './app/config.php';
-require_once './app/autoload.php';
 require_once './app/Functions.php';
 require_once './app/Route.php';
